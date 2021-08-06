b9c4b12e4924e48000db7e75c09b1f2028cdfa28
hmlu36 <hmlu36@gmail.com>
2021-08-06 14:54:36 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production' ? '/vite-materializecss-stock/' : './',
-  server: {
-    fs: {
-      strict: false,
-    },
-  },
 })
